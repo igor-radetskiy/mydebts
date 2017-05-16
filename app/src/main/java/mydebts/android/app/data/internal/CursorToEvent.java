2b0a44984387922e5b9397b7c00bf9d60e540cfb
Igor Radetskyi <igor.radetskyi@lookout.com>
2017-05-16 18:10:59 -0400

    todo: fix join conflicts

--- app/src/main/java/mydebts/android/app/data/internal/CursorToEvent.java
+++ app/src/main/java/mydebts/android/app/data/internal/CursorToEvent.java
@@ -6,13 +6,17 @@ import java.util.Date;
 
 import io.reactivex.functions.Function;
 import mydebts.android.app.data.db.EventContract;
+import mydebts.android.app.data.db.ParticipantContract;
 import mydebts.android.app.data.model.Event;
 
 class CursorToEvent implements Function<Cursor, Event> {
     @Override
     public Event apply(Cursor cursor) throws Exception {
+        int idColumnIndex = cursor.getColumnIndex(ParticipantContract.COLUMN_EVENT_ID) >= 0 ?
+                cursor.getColumnIndex(ParticipantContract.COLUMN_EVENT_ID) : cursor.getColumnIndex(EventContract._ID);
+
         return Event.builder()
-                .id(cursor.getLong(cursor.getColumnIndex(EventContract._ID)))
+                .id(cursor.getLong(idColumnIndex))
                 .name(cursor.getString(cursor.getColumnIndex(EventContract.COLUMN_NAME)))
                 .date(new Date(cursor.getLong(cursor.getColumnIndex(EventContract.COLUMN_DATE))))
                 .build();
